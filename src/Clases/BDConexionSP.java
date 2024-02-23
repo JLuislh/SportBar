47d2e26598556ad3e97707ef508311cfbd232a57
jluis <jluis@TI.Anatekelectronics.local>
2024-02-23 06:35:21 -0600

    Modificaciones 23/02/24

--- src/Clases/BDConexionSP.java
+++ src/Clases/BDConexionSP.java
@@ -17,9 +17,9 @@ import javax.swing.JOptionPane;
 public class BDConexionSP {
     
       
-    //public static final String URL = "jdbc:mysql://localhost:3306/sportbar?useTimezone=true&serverTimezone=UTC";
+    public static final String URL = "jdbc:mysql://localhost:3306/sportbar?useTimezone=true&serverTimezone=UTC";
     //public static final String URL = "jdbc:mysql://26.177.160.206:3306/sportbar?useTimezone=true&serverTimezone=UTC";
-    public static final String URL = "jdbc:mysql://26.252.190.76:3306/sportbar?useTimezone=true&serverTimezone=UTC";
+    //public static final String URL = "jdbc:mysql://26.252.190.76:3306/sportbar?useTimezone=true&serverTimezone=UTC";
     public static final String USER = "amapola";
     public static final String CLAVE = "sportbar";
      
